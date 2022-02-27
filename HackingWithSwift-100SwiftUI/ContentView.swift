3fe3726e8b15c1ac395d240676f3d96ea53af45b
Mitch Rogers <mitchellmel@outlook.com>
2022-02-27 09:59:38 -0700

    Initial Commit

---
+++ HackingWithSwift-100SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackingWithSwift-100SwiftUI
+//
+//  Created by mitch on 2/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackingWithSwift-100SwiftUI/HackingWithSwift_100SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_100SwiftUIApp.swift
+//  HackingWithSwift-100SwiftUI
+//
+//  Created by mitch on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_100SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
