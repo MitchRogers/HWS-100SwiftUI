9789da036cd2c0a913882982015957b524f35bd9
Mitch Rogers <mitchellmel@outlook.com>
2022-05-04 22:29:41 -0600

    Project 4, BetterRest app

---
+++ HackingWithSwift-100SwiftUI/Projects/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mitch Rogers on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
